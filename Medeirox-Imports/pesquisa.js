71b635afcd51ffc4e2babe9d4eb46509e992c283
Pedro Henrique Barbosa Jatobá <102997365+Pedrenrike@users.noreply.github.com>
2023-07-06 09:51:38 -0300

    Update pesquisa.js

--- Medeirox-Imports/pesquisa.js
+++ Medeirox-Imports/pesquisa.js
@@ -1,23 +1,27 @@
 function search() {
-    let input = document.getElementById('pesquisa').value
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName('nome-produto');
-    let n = 0
-
-    for (i = 0; i < x.length; i++){
-        if(x[i].innerHTML.toLocaleLowerCase().includes(input)){
-            n++;
-        }
+    let input = document.getElementById('pesquisa').value 
+    if (!input){
+        alert("Digite o que desejas pesquisar!");
     }
-
-    alert("Encontrei "+ n +" resultados!");
+    else{
+        atalho(input);
+    }
 }
 
-function atalho (search){
+function atalho(search){
     sessionStorage.setItem('search', search);
+    window.location.href = "pesquisa.html";
 }
 
 function pesquisa(){
     let search = sessionStorage.getItem('search');
-    alert(search);
+    search = search.toLowerCase();
+    x = document.getElementsByClassName("tags");
+    produto = document.getElementsByClassName("produtos");
+
+    for(i = 0; i < x.length; i++){
+        if(!x[i].innerHTML.toLowerCase().includes(search)){
+            produto[i].style.display="none";
+        }   
+    }
 }
